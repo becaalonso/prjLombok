373be85005688f12bd872f6e712587a496f096b0
becaalonso <133991110+becaalonso@users.noreply.github.com>
2023-11-16 08:03:47 -0300

    Add files via upload

---
+++ src/main/java/com/Rebeca/lombok/projetoLombok/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.Rebeca.lombok.projetoLombok.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Rebeca.lombok.projetoLombok.entities.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
+	
+
